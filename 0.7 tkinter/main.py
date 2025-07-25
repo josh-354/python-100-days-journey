40a8bccadf4088ea0192f90d816b5c28a9c54e4c
josh-354 <bryllejoshua354@gmail.com>
2025-07-25 16:28:29 +0800

    hi

--- 0.7 tkinter/main.py
+++ 0.7 tkinter/main.py
@@ -4,10 +4,10 @@ window = tkinter.Tk()#creating a window
 window.title("My first GUI program")#tittle name
 window.minsize(width=500,height=300)#sets the minimum size of the window
 
+my_label = tkinter.Label(text="I love you",font=("Arial",24,"bold"))
+my_label.pack(side="left")#place the label in the sccreen
 
-my_label = tkinter.Label(text="I am label")
-my_label.pack()#place the label in the sccreen
-
+import turtle
 
 
 window.mainloop()
