60a9bd348dd154594a00da219d788f5dc529f950
Brylle Joshua <bryllejoshua354@gmail.com>
2025-08-08 19:13:59 +0300

    yes

--- 0.10/main.py
+++ 0.10/main.py
@@ -1,6 +1,22 @@
-try:
-    file = open ("file.txt")
-    a
-except:
-    file= open("file.txt","w")
-    file.write("")
+import pandas
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+IMAGE_PATH = os.path.join(BASE_DIR, "nato_phonetic_alphabet.csv")
+
+data = pandas.read_csv(IMAGE_PATH)
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
