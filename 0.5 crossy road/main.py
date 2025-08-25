be802a986e8acc86aefb1f8a8e7c9384b32ab200
Brylle Joshua <bryllejoshua354@gmail.com>
2025-08-25 11:20:40 +0800

    update

--- 0.5 crossy road/main.py
+++ 0.5 crossy road/main.py
@@ -1,28 +1,38 @@
-from turtle import Turtle, Screen
 import time
-from car_manager import Car_manager
+from turtle import Screen
 from player import Player
+from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(height=600,width=600)
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
 screen.tracer(0)
 
-screen.listen()
- 
-player=Player()
-car_manager = Car_manager()
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
 
 screen.listen()
-screen.onkey(player.go_up,"w")
+screen.onkeypress(player.move_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-    car_manager.create_cars()
-    car_manager.move_cars()
+    car_manager.add_car()
+    car_manager.move_car()
+
+    if player.ycor() > 310:
+        scoreboard.player_point()
+        player.starting_position()
+        car_manager.increase_speed()
+
+    for car in car_manager.cars:
+        if player.distance(car) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
